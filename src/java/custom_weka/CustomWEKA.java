a3d8a4c7309f98d8ba47979fb2fdeed625a6d2d2
Rakhmatullah Yoga S <13512053@std.stei.itb.ac.id>
2014-12-01 00:42:08 +0700

    Fixing algoritma learning di program utama, masih gagal classify, kemungkinan keluaran modelnya blm tepat

--- src/java/custom_weka/CustomWEKA.java
+++ src/java/custom_weka/CustomWEKA.java
@@ -10,20 +10,19 @@ package custom_weka;
  * @author Rakhmatullah Yoga Sutrisna - 13512053
  */
 
+import java.io.File;
 import java.util.Random;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
-import weka.classifiers.functions.MultilayerPerceptron;
-import weka.classifiers.lazy.IBk;
 import weka.classifiers.meta.FilteredClassifier;
-import weka.classifiers.trees.J48;
 import weka.core.Attribute;
 import weka.core.Instances;
 import weka.core.SerializationHelper;
 import weka.core.Stopwords;
 import weka.core.converters.ConverterUtils.DataSink;
 import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.tokenizers.WordTokenizer;
 import weka.experiment.InstanceQuery;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.NominalToString;
@@ -53,7 +52,7 @@ public class CustomWEKA {
      */
     public Instances ReadfromDatabase(String mQuerry) throws Exception {
         InstanceQuery query;
-        Instances nonSTW;
+        Instances nominal;
         Instances dataset;
         StringToWordVector strToWV;
         query = new InstanceQuery();
@@ -61,13 +60,29 @@ public class CustomWEKA {
         query.setUsername("root");
         query.setPassword("");
         query.setQuery(mQuerry);
-        nonSTW = query.retrieveInstances();
-        // Harus diubah dulu tipe atributnya
+        nominal = query.retrieveInstances();
+        return nominal;
+    }
+    /**
+     * Preproses dataset sebelum membangun model
+     * @param nominal dataset yang atributnya masih berupa nominal
+     * @return dataset yang atributnya telah dibuat menjadi string
+     * @throws Exception 
+     */
+    public Instances Preprocess(Instances nominal) throws Exception {
+        Instances dataset;
         NominalToString filter = new NominalToString();
         filter.setAttributeIndexes("1,2");
-        filter.setInputFormat(nonSTW);
-        dataset = Filter.useFilter(nonSTW, filter);
-        strToWV = new StringToWordVector();
+        filter.setInputFormat(nominal);
+        dataset = Filter.useFilter(nominal, filter);
+        StringToWordVector strToWV = new StringToWordVector();
+        WordTokenizer token = new WordTokenizer();
+        token.setDelimiters(" \r \t.,;:\'\"()?![]1234567890-/");
+        strToWV.setLowerCaseTokens(true);
+        strToWV.setMinTermFreq(5);
+        strToWV.setStopwords(new File("stopwords/stopwordID.txt"));
+        strToWV.setTokenizer(token);
+        strToWV.setWordsToKeep(1000);
         strToWV.setInputFormat(dataset);
         dataset = Filter.useFilter(dataset, strToWV);
         Attribute attr = dataset.attribute("LABEL");
@@ -110,9 +125,25 @@ public class CustomWEKA {
      * @throws Exception 
      */
     public void CreateAndSaveModel(Classifier cls, Instances dataset) throws Exception {
-        clasifier = cls;
+        FilteredClassifier filtercls = new FilteredClassifier();
+        StringToWordVector strToWV = new StringToWordVector();
+        WordTokenizer token = new WordTokenizer();
+        token.setDelimiters(" \r \t.,;:\'\"()?![]1234567890-/");
+        strToWV.setAttributeIndices("1,2");
+        strToWV.setLowerCaseTokens(true);
+        strToWV.setMinTermFreq(5);
+        strToWV.setStopwords(new File("stopwords/stopwordID.txt"));
+        strToWV.setTokenizer(token);
+        strToWV.setWordsToKeep(1000);
+        strToWV.setUseStoplist(true);
+        strToWV.setInputFormat(dataset);
+        filtercls.setClassifier(cls);
+        filtercls.setFilter(strToWV);
+        clasifier = filtercls;
         clasifier.buildClassifier(dataset);
-        SerializationHelper.write("model/"+cls.getClass().getSimpleName()+".model", cls);
+        TenFoldTrain(dataset);
+        FullTraining(dataset);
+        SerializationHelper.write("model/"+cls.getClass().getSimpleName()+".model", clasifier);
     }
     /**
      * Membaca model yang telah dibuat
@@ -129,10 +160,13 @@ public class CustomWEKA {
      */
     public Instances ClassifyUnlabeled() throws Exception {
         Instances dataset;
+        Instances classified;
         dataset = new Instances(unlabeled);
-        for(int i=0; i<unlabeled.numInstances(); i++) {
-            double clsLabel = clasifier.classifyInstance(unlabeled.instance(i));
-            dataset.instance(i).setClassValue(clsLabel);
+        dataset.setClassIndex(dataset.numAttributes()-1);
+        classified = new Instances(dataset);
+        for(int i=0; i<dataset.numInstances(); i++) {
+            double clsLabel = clasifier.classifyInstance(dataset.instance(i));
+            classified.instance(i).setClassValue(clsLabel);
         }
         return dataset;
     }
@@ -164,43 +198,20 @@ public class CustomWEKA {
                 + "FROM (artikel NATURAL JOIN artikel_kategori_verified), kategori "
                 + "WHERE artikel.ID_ARTIKEL=artikel_kategori_verified.ID_ARTIKEL "
                 + "AND kategori.ID_KELAS=artikel_kategori_verified.ID_KELAS;";
-        test.SetLabeled(test.ReadfromDatabase(labeledQuerry));
+        Instances nom = new Instances(test.ReadfromDatabase(labeledQuerry));
+        Instances processed_nom = new Instances(test.Preprocess(nom));
 
         // Membuat model dan menyimpannya, kemudian ditrain
-        test.CreateAndSaveModel(new J48(), test.GetLabeled());                          // J48
-        test.TenFoldTrain(test.GetLabeled());
-        test.FullTraining(test.GetLabeled());
+        NaiveBayes nBayes = new NaiveBayes();
+        nBayes.setUseKernelEstimator(true);
+        test.CreateAndSaveModel(nBayes, processed_nom);
         
-        test.CreateAndSaveModel(new NaiveBayes(), test.GetLabeled());                   // Naive Bayes
-        test.TenFoldTrain(test.GetLabeled());
-        test.FullTraining(test.GetLabeled());
-        
-        test.CreateAndSaveModel(new IBk(), test.GetLabeled());                          // k-NN
-        test.TenFoldTrain(test.GetLabeled());
-        test.FullTraining(test.GetLabeled());
-        
-        /* BEWARE, MODEL INI LAMA BANGET, SUMPAH! 
-        test.CreateAndSaveModel(new MultilayerPerceptron(), test.GetLabeled());         // Multilayer Perceptron
-        test.TenFoldTrain(test.GetLabeled());
-        test.FullTraining(test.GetLabeled()); */
-        
-        /* Mengambil data yang tidak berlabel */
-        // querynya belom dibikin buat nyari yang gak berlabel
-        /*
-        String unlabeledQuerry = "SELECT artikel.JUDUL, artikel.FULL_TEXT, kategori.LABEL "
-                + "FROM (artikel NATURAL JOIN artikel_kategori_verified), kategori "
-                + "WHERE artikel.ID_ARTIKEL=artikel_kategori_verified.ID_ARTIKEL "
-                + "AND kategori.ID_KELAS=artikel_kategori_verified.ID_KELAS;";
-        test.SetUnlabeled(test.ReadfromDatabase(unlabeledQuerry));
-        */
         // Membaca model yang telah disimpan pada file eksternal
-        test.SetModel("model/J48.model");
-        test.SetModel("model/NaiveBayes.model");
-        test.SetModel("model/IBk.model");
-        //test.SetModel("model/MultilayerPerceptron.model");
-    
+        test.SetModel("model/FilteredClassifier.model");
+        
         /* Mengklasifikasikan data yang tidak berlabel */
-        //test.SetLabeled(test.ClassifyUnlabeled());
+        test.SetUnlabeled(test.ReadDataset("dataset/unlabeled.arff"));
+        test.SetLabeled(test.ClassifyUnlabeled());
         
         /* Output hasil klasifikasi */
         DataSink.write("dataset/NewsLabeled.arff", test.GetLabeled());
